9680e1640224bfeaf2ba99621d959a6af8163918
singhvikas <vikas.vksingh@gmail.com>
2018-08-26 11:41:40 -0700

    Using BASE to add Email

--- submissions/chainResearch/baseApi/src/app/models/displayOptions.ts
+++ submissions/chainResearch/baseApi/src/app/models/displayOptions.ts
@@ -2,11 +2,13 @@
   wallet: Boolean;
   rewards: Boolean;
   kyc: Boolean;
+  email: String;
 
   constructor(obj) {
     this.wallet = obj && !!obj.display_wallet || false;
     this.rewards = obj && !!obj.display_rewards || false;
     this.kyc = obj && !!obj.display_kyc || false;
+    this.email = (obj && !!obj.display_email) ? '' : obj.display_email ;
   }
   static get key(): string {
     return 'vault_preferences';
@@ -16,11 +18,13 @@
       display_wallet: this.wallet ? 1 : 0,
       display_rewards: this.rewards ? 1 : 0,
       display_kyc: this.kyc ? 1 : 0,
+      display_email: this.email ? this.email : '',
     });
   }
   updateValue(obj) {
     this.wallet = obj && !!obj.display_wallet || false;
     this.rewards = obj && !!obj.display_rewards || false;
     this.kyc = obj && !!obj.display_kyc || false;
+    this.email = (obj && !!obj.display_email) ? obj.display_email : '' ;
   }
 }
